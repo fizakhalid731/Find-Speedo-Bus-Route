81a50c0e627e968df5d63a8a0e7d17dca8f51565
Fiza Khalid <fiza.khalid731@gmail.com>
2024-10-21 10:01:46 +0500

    Find Speedo Bus Routes

---
+++ script.js
@@ -0,0 +1,543 @@
+let routes = [
+    {
+        routeNumber: 1,
+        Type_of_bus: "Standard bus",
+        stations: [
+            "Railway Station",
+            "Ek Moriya",
+            "Nawaz Sharif Hospital",
+            "Kashmiri Gate",
+            "Lari Adda",
+            "Azadi Chowk",
+            "Texali Chowk",
+            "Bhatti Chowk"
+        ]
+    },
+    {
+        routeNumber: 2,
+        Type_of_bus: "Standard bus",
+        stations: [
+            "Samanabad Morr",
+            "Corporation Chowk",
+            "Taj Company",
+            "Sanda",
+            "Double Sarkan",
+            "Moon Market",
+            "Ganda Nala",
+            "Bhatti Chowk"
+        ]
+    },
+    {
+        routeNumber: 3,
+        Type_of_bus: "Standard bus",
+        stations: [
+        "Railway Station",
+        "Ek Moriya",
+        "Nawaz Sharif Hospital",
+        "Kashmiri Gate",
+        "Lari Adda",
+        "Azadi Chowk",
+        "Timber Market",
+        "METRO",
+        "Niazi Chowk",
+        "Shahdara Metro Station",
+        "Shahdara Lari Adda"
+        ]
+    },
+    {
+        routeNumber: 4,
+        Type_of_bus: "Standard bus",
+        stations: [
+            "R.A Bazar",
+            "Nadeem Chowk",
+            "Defence Morr",
+            "Shareef Market",
+            "Walton",
+            "Qainchi",
+            "Ghazi Chowk",
+            "Chungi Amar Sidhu"
+        ]
+    },
+    {
+      routeNumber: 5,
+      Type_of_bus: "Mini bus",
+      stations: [
+        "Shad Bagh Underpass",
+        "Rajput Park",
+        "Madina Chowk",
+        "Lohay Wali Pulley",
+        "Badami Bagh",
+        "Lari Adda Gol Chakar",
+        "Azadi Chowk",
+        "Taxali Chowk",
+        "Bhatti Chowk"
+      ]
+  },
+//   {
+//     routeNumber: 6,
+//     Type_of_bus: "Mini bus",
+//     stations: [
+//       "Babu Sabu",
+//         "Niazi Adda",
+//         "City Bus Stand",
+//         "Chowk Yateem Khana",
+//         "Bhala Stop",
+//         "Samanabad Morr",
+//         "Chauburji",
+//         "Riwaz Garden",
+//         "M.A.O College",
+//         "Firdous Cinema",
+//         "Raj Garh Chowk"
+//     ]
+// },
+// {
+//   routeNumber: 7,
+//   Type_of_bus: "Standard bus",
+//   stations: [
+//     "Bagrian",
+//     "Depot Chowk",
+//     "Minhaj University",
+//     "Hamdard Chowk",
+//     "Rehmat Eye Hospital",
+//     "Pindi Stop",
+//     "Peco Morr",
+//     "Kot Lakhpat Railway Station",
+//     "Phatak Mandi",
+//     "Qainchi",
+//     "Ghazi Chowk",
+//     "Chungi Amar Sidhu"
+//   ]
+// },
+// {
+//   routeNumber: 8,
+//   Type_of_bus: "Standard bus",
+//   stations: [
+//     "Doctor Hospital",
+//     "Wafaqi Colony",
+//     "IBA Stop",
+//     "Hailey College",
+//     "Campus Pull",
+//     "Barkat Market",
+//     "Kalma Chowk",
+//     "Qaddafi Stadium",
+//     "Canal"
+//   ]
+// },
+// {
+//   routeNumber: 9,
+//   Type_of_bus: "Mini bus",
+//   stations: [
+//     "Railway Station",
+//     "Haji Camp",
+//     "Shimla Pahari",
+//     "Lahore Zoo",
+//     "Chairing Cross",
+//     "Ganga Ram Hospital",
+//     "Qartaba Chowk",
+//     "Chauburji",
+//     "Sham Nagar"
+//   ]
+// },
+// {
+//   routeNumber: 10,
+//   Type_of_bus: "Standard bus",
+//   stations: [
+//     "Multan Chungi",
+//     "Mustafa Town",
+//     "Karim Block Market",
+//     "PU Examination Center",
+//     "Bhekewal Morr",
+//     "Wahdat Colony",
+//     "Naqsha Stop",
+//     "Canal",
+//     "Ichra",
+//     "Shama",
+//     "Qartaba Chowk"
+//   ]
+// },
+// {
+//   routeNumber: 11,
+//   Type_of_bus: "Standard bus",
+//   stations: [
+//     "Babu Sabu",
+//     "Niazi Adda",
+//     "City Bus Stand",
+//     "Chowk Yateem Khana",
+//     "Scheme Morr",
+//     "Flat Stop",
+//     "Dubai Chowk",
+//     "Bhekewal Morr",
+//     "Sheikh Zaid Hospital",
+//     "Campus Pull",
+//     "Barkat Market",
+//     "Kalma Chowk",
+//     "Liberty Chowk",
+//     "Hafeez Center",
+//     "Mini Market",
+//     "Main Market, Gulberg"
+//   ]
+// },
+// {
+//   routeNumber: 12,
+//   Type_of_bus: "Standard bus",
+//   stations: [
+//     "R.A Bazar",
+//     "PAF Market",
+//     "Girja Chowk",
+//     "Afshan Chowk",
+//     "Fortress Stadium",
+//     "Gymkhana",
+//     "Aitchison College",
+//     "PC Hotel",
+//     "Lahore Zoo",
+//     "Chairing Cross",
+//     "GPO",
+//     "Anarkali",
+//     "Civil Secretariat"
+//   ]
+// },
+// {
+//   routeNumber: 13,
+//   Type_of_bus: "Standard bus",
+//   stations: [
+//     "Bagrian",
+//     "Ghazi Chowk",
+//     "UMT Stop",
+//     "Khokhar Chowk",
+//     "Akbar Chowk",
+//     "Pindi Stop",
+//     "Peco Morr",
+//     "Phatak Mandi",
+//     "Ittefaq Hospital",
+//     "Model Town",
+//     "Kalma Chowk"
+//   ]
+// },
+// {
+//   routeNumber: 14,
+//   Type_of_bus: "Standard bus",
+//   stations: [
+//     "R.A Bazar",
+//     "Fauji Foundation",
+//     "Ali View Garden",
+//     "Bhatta Chowk",
+//     "DHA Nursery",
+//     "LESCO",
+//     "Chota Ishara Stop",
+//     "Naka Stop",
+//     "Ghazi Chowk",
+//     "Chungi Amar Sidhu"
+//   ]
+// },
+// {
+//   routeNumber: 15,
+//   Type_of_bus: "Mini bus",
+//   stations: [
+//     "Qartba Chowk",
+//     "Hakeem M. Ajmal Khan Road",
+//     "Gulshan Ravi Road",
+//     "Kacha Ferozepur Road",
+//     "Babu Sabu"
+//   ]
+// },
+// {
+//   routeNumber: 16,
+//   Type_of_bus: "Mini bus",
+//   stations: [
+//     "Railway Station",
+//     "Circular Road",
+//     "Ek Moriya",
+//     "Bhatti Chowk"
+//   ]
+// },
+// {
+//   routeNumber: 17,
+//   Type_of_bus: "Standard bus",
+//   stations: [
+//     "Canal",
+//     "Main Boulevard Shadman",
+//     "Davis Road",
+//     "Shimla Pahari",
+//     "Haji Camp",
+//     "Railway Station"
+//   ]
+// },
+// {
+//   routeNumber: 18,
+//   Type_of_bus: "Mini bus",
+//   stations: [
+//     "Bhatti Chowk",
+//     "Circular Road",
+//     "Nisbat Road",
+//     "Abbot Road",
+//     "Shimla Pahari"
+//   ]
+// },
+// {
+//   routeNumber: 19,
+//   Type_of_bus: "Mini bus",
+//   stations: [
+//     "Main Market",
+//     "Jail Road",
+//     "Lytton Road",
+//     "Crust Road",
+//     "Lower Mall Road",
+//     "Bhatti Chowk"
+//   ]
+// },
+// {
+//   routeNumber: 20,
+//   Type_of_bus: "Mini bus",
+//   stations: [
+//     "Jain Mandar",
+//     "Al-Mumtaz Road",
+//     "Poonch Road",
+//     "Lake Road",
+//     "Chowk Yateem Khana"
+//   ]
+// },
+// {
+//   routeNumber: 21,
+//   Type_of_bus: "Standard bus",
+//   stations: [
+//     "Depot Chowk",
+//     "Madar-e-Millat Road",
+//     "Ali Road",
+//     "Baig Road",
+//     "Canal Bank Road",
+//     "Thokar Niaz Baig"
+//   ]
+// },
+// {
+//   routeNumber: 22,
+//   Type_of_bus: "Standard bus",
+//   stations: [
+//     "Depot Chowk",
+//     "Madar-e-Millat Road",
+//     "Sutlej Avenue",
+//     "Shahrah Nazria-e- Pakistan Avenue",
+//     "Thokar Niaz Baig"
+//   ]
+// },
+// {
+//   routeNumber: 23,
+//   Type_of_bus: "Mini bus",
+//   stations: [
+//     "Valencia",
+//     "Valencia Main Boulevard",
+//     "Khayaban-e-Jinnah",
+//     "Raiwind Road",
+//     "Thokar Niaz Baig"
+//   ]
+// },
+// {
+//   routeNumber: 24,
+//   Type_of_bus: "Standard bus",
+//   stations: [
+//     "Multan Chungi",
+//     "College Road",
+//     "Maulana Shaukat Ali Road",
+//     "Wahdat Road",
+//     "Ghazi Chowk"
+//   ]
+// },
+// {
+//   routeNumber: 25,
+//   Type_of_bus: "Standard bus",
+//   stations: [
+//     "R.A Bazar",
+//     "Lahore-Bedian Road",
+//     "Allama Iqbal Road",
+//     "Railway Station"
+//   ]
+// },
+// {
+//   routeNumber: 26,
+//   Type_of_bus: "Standard bus",
+//   stations: [
+//     "R.A Bazar",
+//     "G.T Road",
+//     "Shalimar Link Road",
+//     "Tufail Road",
+//     "Sarfraz Rafique Road",
+//     "Daroghawala"
+//   ]
+// },
+// {
+//   routeNumber: 27,
+//   Type_of_bus: "Mini bus",
+//   stations: [
+//     "BataPur",
+//     "GT Road",
+//     "Daroghawala"
+//   ]
+// },
+// {
+//   routeNumber: 28,
+//   Type_of_bus: "Mini bus",
+//   stations: [
+//     "Quaid e Azam Interchange",
+//     "Harbanspura Road",
+//     "Zarar Shaheed Road",
+//     "Airport"
+//   ]
+// },
+// {
+//   routeNumber: 29,
+//   Type_of_bus: "Standard bus",
+//   stations: [
+//     "Niazi Interchange",
+//     "Lahore Ring Road",
+//     "Band Road",
+//     "Sue Wala Road",
+//     "Salamat Pura"
+//   ]
+// },
+// {
+//   routeNumber: 30,
+//   Type_of_bus: "Standard bus",
+//   stations: [
+//     "Daroghawala",
+//     "G.T. Road",
+//     "Shalimar Link Road",
+//     "Airport"
+//   ]
+// },
+// {
+//   routeNumber: 31,
+//   Type_of_bus: "Mini bus",
+//   stations: [
+//     "Daroghawala",
+//     "Chamra Mandi",
+//     "Cooper Store",
+//     "UET",
+//     "Shalimar Chowk",
+//     "Lari Adda"
+//   ]
+// },
+// {
+//   routeNumber: 32,
+//   Type_of_bus: "Mini bus",
+//   stations: [
+//     "Shimla Pahari",
+//     "Durand Road",
+//     "Queen Mary Road",
+//     "Garhi Shahu Bridge",
+//     "Cooper Store",
+//     "Chamra Mandi",
+//     "Ek Moriya"
+//   ]
+// },
+// {
+//   routeNumber: 33,
+//   Type_of_bus: "Mini bus",
+//   stations: [
+//     "Cooper Store",
+//     "Workshop Road",
+//     "Mughalpura Road",
+//     "Mughalpura"
+//   ]
+// },
+// {
+//   routeNumber: 34,
+//   Type_of_bus: "Mini bus",
+//   stations: [
+//     "Singhpura",
+//     "Wheatman Road",
+//     "Griffin Road",
+//     "Mughalpura"
+//   ]
+// }
+];
+
+
+
+
+
+// dropdownlist func//
+const dropdownlist = () => {
+    let from_stations = document.getElementById("from-station");
+    let to_stations = document.getElementById("to-station");
+
+    let from_Value = from_stations.value;
+    let to_Value = to_stations.value;
+
+
+    for (let i = 0; i < from_stations.length; i++) {
+      from_stations[i].disabled = false;
+    }
+
+    for (let i = 0; i < to_stations.length; i++) {
+      to_stations[i].disabled = false;
+    }
+
+    if (from_Value) {
+      for (let i = 0; i < from_stations.length; i++) {
+        if (from_stations[i].value == to_Value) {
+          from_stations[i].disabled = true;
+        }
+      }
+    }
+
+    if (to_Value) {
+      for (let i = 0; i < to_stations.length; i++) {
+        if (to_stations[i].value == from_Value) {
+          to_stations[i].disabled = true;
+        }
+      }
+    }
+  };
+
+// linear search
+  const show_Route_with_linear_search = () => {
+    let from_stations = document.getElementById("from-station");
+    let to_stations = document.getElementById("to-station");
+
+    let from_Value = from_stations.value;
+    let to_Value = to_stations.value;
+
+    let div_result = document.getElementById("result");
+    div_result.innerHTML = ""; // Clear previous results
+
+
+    // Use map to find routes
+    let find_route = routes.map(route => {
+        let from_station_Exists = false; 
+        let to_station_Exists = false;
+        
+        route.stations.map(find_station => {
+            if(find_station == from_Value){
+                from_station_Exists = true;
+            }
+
+            if(find_station == to_Value){
+                to_station_Exists = true;
+            }
+        });
+         
+        return from_station_Exists && to_station_Exists ? route : null;
+
+    }).filter(route => route !== null); // Filter out null values
+
+    // Display the results
+    if (find_route.length > 0) {
+        find_route.map(route => {
+            div_result.innerHTML += `
+            
+                <h3>Route Number: ${route.routeNumber}</h3>
+                <h4>From: ${from_Value} <i class="fa-solid fa-arrow-right"></i> To: ${to_Value}</h4>
+                <h4>Bus Type: ${route.Type_of_bus}</h4>
+                <p>
+                    ${route.stations.map(station => `${station}`).join(` <i class="fa-solid fa-arrow-right"></i> `)}
+                </p>
+              
+            `;
+        });
+    } else {
+        div_result.innerHTML = "<p>No route found for the selected options.</p>";
+    }
+};
+
+
+
+
